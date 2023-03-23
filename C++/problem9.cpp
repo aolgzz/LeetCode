3e0010632a28ffdb933d15f828e2553ea6acd408
Acutis++ <113034629+aolgzz@users.noreply.github.com>
2023-03-23 17:16:55 -0600

    Added (best) arithmetic approach to Problem 9

--- C++/problem9.cpp
+++ C++/problem9.cpp
@@ -30,6 +30,34 @@ bool isPalindrome(int num){
 
 }
 
+//Arithmetic approach - better solution, no use of any data strcture.
+bool isPalindrome2(int num){
+
+    if(num < 0 || (num % 10 == 0 && num != 0)){
+    /*
+        Restrictions to num:
+            - Negative numbers can't be palindrome numbers
+            - Numbers ending with 0 can't be palindrome numbers,
+              except zero itself.
+    
+    */
+        return false;
+    }
+
+    int reversed = 0; // Initialize a variable to store the reversed right half of the number
+
+    while(num > reversed){ //This condition is what determines from which digit is the half
+        reversed = reversed * 10 + num % 10; // Reverse the digits of the right half of the number and store them in 'reversed'
+        num /= 10; // Remove the last digit from the left half of the number
+    }
+
+
+    // Compare the left half of the number to the reversed right half to determine if it's a palindrome
+    return num == reversed || num == reversed / 10;
+
+
+}
+
 int main(){
 
     std::vector<int>fake_str, reversed_str;
